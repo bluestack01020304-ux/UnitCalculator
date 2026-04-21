0443014d411f361d1ba9999dc0602a0cf9987695
jenopa <bluestack01020304@gmail.com>
2026-04-21 15:15:30 +0900

    feat: 등급 계산 핢수

--- Utillity/Scroes.py
+++ Utillity/Scroes.py
@@ -5,17 +5,17 @@ def getScores(number: int):
     int만큼에 국수영사과에 점수가 랜덤으로 뽑힘"""
     scores = []
     for i in range(number):
-        scores.append({
-            "국어": random.randint(0,100), # 국어
-            "수학": random.randint(0,100), # 수학
-            "영어": random.randint(0,100), # 영어
-            "사회": random.randint(0,100), # 사회
-            "과학": random.randint(0,100), # 과학
-        })
+        scores.append([
+            random.randint(40,100), # 국어
+            random.randint(40,100), # 수학
+            random.randint(40,100), # 영어
+            random.randint(40,100), # 사회
+            random.randint(40,100), # 과학
+            ])
 
     return scores
 
 def readScores(data: list):
     for v in data:
-        for i, s in v.items():
-            print(f"{i}: {s}")
+        for i in v:
+            print(i, end=" ")
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import backend.config as config
 def start():
     config.scores = Scores.getScores(100)
 
-    userScore = list(map(int, input().split()))
-    Scores.readScores(config.scores)
-
-    print(config.scores[0]["국어"])
+    score = list(map(int, input().split()))
+    print("당신의 평균: ", Math.avg(score))
+    print("당신의 등급: ", Math.grade(score))
+    print("당신의 석차: ", Math.ranking(score))
 
 if __name__ == "__main__":
     start()
